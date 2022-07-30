b54b937da9be5b9264fe13fb4b92270f4e8221fb
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2022-07-29 23:35:00 -0300

    primeiro commit

---
+++ Fundamentos/inicio/bloco.js
@@ -0,0 +1,37 @@
+//bloco sem propósito
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
+
+{
+    ;
+    ;
+    ;
+}
+
+{
+    {
+        {
+            {
+                {
+                    {
+                        ;
+                        ;
+                        ;
+                    }
+                }
+            }
+        }
+    }
+}
+
+{}
+console.log("Fim!");
+//Organizado em pastas/arquivos/sentenças de códigos e blocos
---
+++ Fundamentos/inicio/sentença_2.js
@@ -0,0 +1,4 @@
+console.log("Bom dia pessoal");
+console.log(2);
+console.log(3+6);
+console.log('Assim também!');
